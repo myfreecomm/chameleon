a017341c8da5603831de4c59fc262fc7174833f4
Rodrigo Tassinari de Oliveira <rodrigo@pittlandia.net>
2015-03-04 18:24:13 -0300

    sugere iteracao para o hover no menu do breadcrumb e incorpora alguns placeholders e mixins

--- src/common/_header.php
+++ src/common/_header.php
@@ -1,8 +1,14 @@
 <header class="header">
-  <a href="#menu"></a>
+  <a href="#menu" class="header-button-menu"></a>
   <ul class="header-breadcrumb">
-    <li class="header-breadcrumb-list">Emites</li>
+    <li class="header-breadcrumb-list">
+    	<a href="#" class="header-breadcrumb-link">Emites</a>
+    	<i class="fa fa-angle-down"></i>
+    </li>
     <i class="fa fa-angle-right"></i>
-    <li class="header-breadcrumb-list">Nota Fiscal</li>
+    <li class="header-breadcrumb-list">
+    	<a href="#" class="header-breadcrumb-link">Nota Fiscal</a>
+    	<i class="fa fa-angle-down"></i>
+    </li>
   </ul>
 </header>
