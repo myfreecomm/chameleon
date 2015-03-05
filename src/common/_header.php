585a9f672904b2458d7977c5047423b861098568
Rodrigo Tassinari de Oliveira <rodrigo@pittlandia.net>
2015-03-05 00:21:59 -0300

    altera textos e inclui links

--- src/common/_header.php
+++ src/common/_header.php
@@ -2,13 +2,16 @@
   <a href="#menu" class="header-button-menu"></a>
   <ul class="header-breadcrumb">
     <li class="header-breadcrumb-list">
-    	<a href="#" class="header-breadcrumb-link">Emites</a>
+    	<a href="#" class="header-breadcrumb-link">Chameleon</a>
     	<i class="fa fa-angle-down"></i>
     </li>
     <i class="fa fa-angle-right"></i>
     <li class="header-breadcrumb-list">
-    	<a href="#" class="header-breadcrumb-link">Nota Fiscal</a>
+    	<a href="#" class="header-breadcrumb-link">Framework</a>
     	<i class="fa fa-angle-down"></i>
     </li>
   </ul>
+  <i class="fa fa-search right"></i>
+  &nbsp;
+  <i class="fa fa-info-circle right"></i>
 </header>
--- src/common/_nav.php
+++ src/common/_nav.php
@@ -1,22 +1,18 @@
 <nav id="menu">
   <ul>
-    <li><a class="nav-link" href="#">NFe</a></li>
-    <li><a class="nav-link" href="#">NFSe</a></li>
-    <li><a class="nav-link" href="#">Lotes</a></li>
+    <li><a class="nav-link" href="#">Grid</a></li>
+    <li><a class="nav-link" href="#">Tabela</a></li>
+    <li><a class="nav-link" href="#">Inputs</a></li>
 
-    <li class="Label">Cadastros</li>
-    <li class="Collapsed"> <a class="nav-link" href="#">Emitentes</a></li>
-    <li class="Collapsed"><a class="nav-link" href="#">Tomadores</a></li>
-    <li class="Collapsed"><a class="nav-link" href="#">Serviços</a></li>
-    <li class="Collapsed"><a class="nav-link" href="#">Produtos</a></li>
+    <li class="Label">Plugins</li>
+    <li class="Collapsed"> <a class="nav-link" href="#">Mmenu</a></li>
+    <li class="Collapsed"><a class="nav-link" href="#">Jquery</a></li>
+    <li class="Collapsed"><a class="nav-link" href="#">Foundation</a></li>
 
-    <li class="Label">Gerenciamento</li>
-    <li class="Collapsed"><a class="nav-link" href="#">Gerenciar Conta</a></li>
-    <li class="Collapsed"><a class="nav-link" href="#">Configurações da API</a></li>
-    <li class="Collapsed"><a class="nav-link" href="#">Configurações de Webhook</a></li>
+    <li class="Label">Workflow</li>
+    <li class="Collapsed"><a class="nav-link" href="#">Grunt</a></li>
+    <li class="Collapsed"><a class="nav-link" href="#">SASS</a></li>
+    <li class="Collapsed"><a class="nav-link" href="#">Compass</a></li>
     
-    <li class="Label">Teste</li>
-    <li class="Collapsed"><a class="nav-link" target="_blank" href="#">Editar Perfil</a></li>
-    <li class="Collapsed"><a class="nav-link" href="#">Sair</a></li>
   </ul>
 </nav>
