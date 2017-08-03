0965219579b6ed76aef0122c6efd43b0868c6017
Thiago Thimóteo <thiago.thimoteo@gmail.com>
2017-08-03 16:44:48 -0300

    Fake Nexaas app folder structure

---
+++ nexaas-app/gulpfile.js
@@ -0,0 +1,28 @@
+var gulp = require('gulp');
+var less = require('gulp-less');
+var minifyCSS = require('gulp-csso');
+var concat = require('gulp-concat');
+var browserSync = require('browser-sync').create();
+
+gulp.task('css', function(){
+  return gulp.src('app/assets/less/**/*.less')
+    .pipe(less())
+    .pipe(minifyCSS())
+    .pipe(gulp.dest('app/assets/css'))
+    .pipe(browserSync.stream());
+});
+
+// Static Server + watching scss/html files
+gulp.task('serve', ['css'], function() {
+
+    browserSync.init({
+        server: "./app",
+        notify: false
+    });
+
+    gulp.watch("app/assets/less/**/*.less", ['css']);
+    gulp.watch("app/*.html").on('change', browserSync.reload);
+    gulp.watch("app/assets/**/*.js").on('change', browserSync.reload);
+});
+
+gulp.task('default', ['serve']);
