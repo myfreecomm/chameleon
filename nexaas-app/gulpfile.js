ca1f133fbd952e7d2f8c781ce97b7dd4ad2389c7
Thiago Thimóteo <thiago.thimoteo@gmail.com>
2017-08-30 15:19:41 -0300

    Concatenates CSS files

--- nexaas-app/gulpfile.js
+++ nexaas-app/gulpfile.js
@@ -2,6 +2,7 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var minifyCSS = require('gulp-csso');
 var concat = require('gulp-concat');
+var concatCss = require('gulp-concat-css');
 var browserSync = require('browser-sync').create();
 
 gulp.task('css', function(){
@@ -12,17 +13,23 @@ gulp.task('css', function(){
     .pipe(browserSync.stream());
 });
 
+gulp.task('concat', function() {
+  return gulp.src(['app/assets/css/semantic.min.css', 'app/assets/css/style.css'])
+    .pipe(concatCss('main.css'))
+    .pipe(minifyCSS())
+    .pipe(gulp.dest('app/assets/css'));
+});
+
 // Static Server + watching scss/html files
-gulp.task('serve', ['css'], function() {
+gulp.task('serve', ['css', 'concat'], function() {
+  browserSync.init({
+    server: "./app",
+    notify: false
+  });
 
-    browserSync.init({
-        server: "./app",
-        notify: false
-    });
-
-    gulp.watch("app/assets/scss/**/*.scss", ['css']);
-    gulp.watch("app/*.html").on('change', browserSync.reload);
-    gulp.watch("app/assets/**/*.js").on('change', browserSync.reload);
+  gulp.watch("app/assets/scss/**/*.scss", ['css']);
+  gulp.watch("app/*.html").on('change', browserSync.reload);
+  gulp.watch("app/assets/**/*.js").on('change', browserSync.reload);
 });
 
 gulp.task('default', ['serve']);
