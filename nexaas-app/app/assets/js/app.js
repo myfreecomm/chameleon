bff5a3849951c5cfdcbf704b3f31b45511e3815c
Thiago Thimóteo <thiago.thimoteo@gmail.com>
2018-01-16 17:16:50 -0200

    Tiny fix on js

--- nexaas-app/app/assets/js/app.js
+++ nexaas-app/app/assets/js/app.js
@@ -171,14 +171,18 @@ $(window).resize(function() {
 
 var slider = document.getElementById('slider');
 
-noUiSlider.create(slider, {
-  start: [20, 80],
-  connect: true,
-  range: {
-    'min': 0,
-    'max': 100
-  }
-});
+if (slider) {
+  noUiSlider.create(slider, {
+    start: [20, 80],
+    connect: true,
+    range: {
+      'min': 0,
+      'max': 100
+    }
+  });
+}
+
+
 
 /*** SEMANTIC UI **/
 
