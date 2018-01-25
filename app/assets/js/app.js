df83f2ff079e56a31eb7c6aca344a8fd4f5c7102
Thiago Thimóteo <thiago.thimoteo@gmail.com>
2018-01-25 12:58:25 -0200

    Extract Semantic UI js from app.js

--- app/assets/js/app.js
+++ app/assets/js/app.js
@@ -118,7 +118,7 @@ var chameleon = (function(){
   }
 
   var toggleSidebarCollapse = function() {
-    var $container = $(this).prev('.ch-timeline-container',
+    var $container = $(this).prev('.ch-timeline-container'),
         $collapseButton = $(this);
 
     switchClass($collapseButton.find('.icon'), 'right', 'left');
@@ -185,67 +185,3 @@ var chameleon = (function(){
 $(document).ready(function() {
   chameleon.init();
 });
-
-var slider = document.getElementById('slider');
-
-if (slider) {
-  noUiSlider.create(slider, {
-    start: [20, 80],
-    connect: true,
-    range: {
-      'min': 0,
-      'max': 100
-    }
-  });
-}
-
-/*** SEMANTIC UI **/
-
-$('.ui.dropdown')
-  .dropdown({
-    showOnFocus: false
-  })
-;
-
-$('.ui.checkbox')
-  .checkbox()
-;
-
-$('.bind-popup')
-  .popup({
-    inline: true
-  });
-;
-
-$('.ui.info')
-  .popup({
-    inline: true,
-    on: 'hover'
-  })
-;
-
-$('.ui.sticky')
-  .sticky({
-    context: '#report-form'
-  })
-;
-
-$('.menu .item')
-  .tab()
-;
-
-$('#rangestart').calendar({
-  type: 'date',
-  endCalendar: $('#rangeend')
-});
-
-$('#rangeend').calendar({
-  type: 'date',
-  startCalendar: $('#rangestart'),
-  popupOptions: {
-    position: 'bottom right',
-    lastResort: 'bottom right'
-  }
-});
-
-
