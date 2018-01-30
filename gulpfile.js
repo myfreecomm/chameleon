1557bd6b5b094c767abbe3d470fba2395b36f3e5
Thiago Thimóteo <thiago.thimoteo@gmail.com>
2018-01-30 12:27:32 -0200

    Updates JS folder structures

--- gulpfile.js
+++ gulpfile.js
@@ -7,42 +7,44 @@ var rename = require('gulp-rename');
 var concat = require('gulp-concat');
 var babel = require("gulp-babel");
 
+var assetsPath = "source/assets";
+
 gulp.task('css', function(){
-  return gulp.src('app/assets/scss/style.scss')
+  return gulp.src(`${assetsPath}/scss/style.scss`)
     .pipe(sass().on('error', sass.logError))
     .pipe(minifyCSS())
     .pipe(rename('chameleon.min.css'))
-    .pipe(gulp.dest('app/assets/css'))
+    .pipe(gulp.dest(`${assetsPath}/css`))
     .pipe(browserSync.stream());
 });
 
 gulp.task('views', function buildHTML() {
-  return gulp.src('app/assets/pug/*.pug')
+  return gulp.src(`${assetsPath}/pug/*.pug`)
   .pipe(pug({
     pretty: true,
   }))
-  .pipe(gulp.dest('./app'));
+  .pipe(gulp.dest('./source'));
 });
 
 gulp.task('scripts', function() {
-  return gulp.src('app/assets/js/src/**/*.js')
-    .pipe(concat('app.js'))
+  return gulp.src(`${assetsPath}/js/chameleon/**/*.js`)
+    .pipe(concat('main.js'))
     .pipe(babel())
-    .pipe(gulp.dest('app/assets/js/dist/'))
+    .pipe(gulp.dest(`${assetsPath}/js/`))
     .pipe(browserSync.stream());
 });
 
 // Static Server + watching scss/html files
 gulp.task('serve', ['views', 'css', 'scripts'], function() {
   browserSync.init({
-    server: "./app",
+    server: "./source",
     notify: false
   });
 
-  gulp.watch("app/assets/pug/**/*.pug", ['views']);
-  gulp.watch("app/assets/scss/**/*.scss", ['css']);
-  gulp.watch("app/assets/**/*.js", ['scripts']);
-  gulp.watch("app/*.html").on('change', browserSync.reload);
+  gulp.watch(`${assetsPath}/pug/**/*.pug`, ['views']);
+  gulp.watch(`${assetsPath}/scss/**/*.scss`, ['css']);
+  gulp.watch(`${assetsPath}/**/*.js`, ['scripts']);
+  gulp.watch("source/*.html").on('change', browserSync.reload);
 });
 
 gulp.task('default', ['serve']);
