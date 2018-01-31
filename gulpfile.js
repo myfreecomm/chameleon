f500f38c6025a4b5ee28f4d55433f2f0e3356d75
Thiago Thimóteo <thiago.thimoteo@gmail.com>
2018-01-31 19:09:12 -0200

    Adds dist files

--- gulpfile.js
+++ gulpfile.js
@@ -5,20 +5,22 @@ let styles = require('./tasks/styles'),
     templates = require('./tasks/templates'),
     browserSync = require('./tasks/watch');
 
-gulp.task('css', styles.bundle );
+gulp.task('css', styles.bundle);
 
-gulp.task('styles', ['css'], styles.build );
+gulp.task('styles', ['css'], styles.build);
 
-gulp.task('babel', scripts.compile )
+gulp.task('babel', scripts.compile)
 
-gulp.task('scripts', ['babel'], scripts.bundle );
+gulp.task('scripts', ['babel'], scripts.bundle);
 
-gulp.task('cleanJS', scripts.cleanFiles );
+gulp.task('cleanJS', scripts.cleanFiles);
 
-gulp.task('templates', templates.build );
+gulp.task('templates', templates.build);
 
-gulp.task('deployJS', scripts.deploy );
+gulp.task('deployJS', scripts.deploy);
 
-gulp.task('serve', ['templates', 'styles', 'scripts'], browserSync.watch );
+gulp.task('serve', ['templates', 'styles', 'scripts'], browserSync.watch);
 
 gulp.task('default', ['serve']);
+
+gulp.task('deploy', ['deployJS']);
