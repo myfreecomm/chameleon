c61c5abd258a26387de2a171b7dc9b5930bdaf73
paulo70 <paulofrontend@gmail.com>
2018-11-25 19:40:30 -0200

    refactor js flash message add timer to close

--- source/assets/javascripts/chameleon/components/message.js
+++ source/assets/javascripts/chameleon/components/message.js
@@ -1,12 +1,29 @@
 Chameleon.Components.Message = function() {
 
-  const closeButton = document.querySelector('[data-action="close"]');
+	class handleCloseMessage {
 
-  const handleCloseMessage = (element, event, attr) => {
-    element.addEventListener(event, function() {
-      element.parentNode.classList.add(attr);
-    });
-  }
+		constructor(){
+			let $ = document.querySelector.bind(document);
+			this.closeButton = $('[data-action="close"]');
+			this.contentFlashMessage = $('[data-action="content"]');
+		}
 
-  handleCloseMessage(closeButton, 'click', 'ch-message--hide');
+		 _addEventToClose(){
+			this.closeButton.addEventListener('click', function(){
+				this.parentNode.classList.add('ch-message--hide');
+			})
+		}
+
+		 _addTimerToClose(){
+			setTimeout(() =>{ 
+				this.contentFlashMessage.classList.add('ch-message--hide');
+			}, 4000);
+		}
+	}
+
+
+	let callCloseMessage = new handleCloseMessage();
+	
+	callCloseMessage._addEventToClose();
+	callCloseMessage._addTimerToClose();
 }
