6f61ed853eaf2e4dbfee9eb0937784676764d678
Thiago Thimóteo <thiago.thimoteo@gmail.com>
2019-04-03 16:58:38 -0300

    Initial webpack configuration

---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+const config = {
+  mode: 'development',
+  entry: './examples/index.js',
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'examples')
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+      chunkFilename: "[id].css"
+    }),
+    new HtmlWebpackPlugin({
+      template: './source/templates/dashboard.pug'
+    }),
+  ],
+  module: {
+    rules: [
+      {
+       test: /\.(sa|sc|c)ss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'sass-loader',
+        ]
+      },
+      {
+        test: /\.pug$/,
+        use: ["pug-loader"]
+      }
+    ]
+  },
+  devServer: {
+    contentBase: './examples'
+  },
+};
+
+module.exports = config
