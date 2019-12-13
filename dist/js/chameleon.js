e133b604ca195179e73fd4b47050f729bf1480e0
Thiago Thimóteo <thiago.thimoteo@gmail.com>
2019-12-13 15:49:35 -0300

    Refactor breakpoints to beta version

--- dist/js/chameleon.js
+++ dist/js/chameleon.js
@@ -86,6 +86,17 @@
 /************************************************************************/
 /******/ ({
 
+/***/ "./scss/beta/index.scss":
+/*!******************************!*\
+  !*** ./scss/beta/index.scss ***!
+  \******************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+eval("// extracted by mini-css-extract-plugin\n\n//# sourceURL=webpack:///./scss/beta/index.scss?");
+
+/***/ }),
+
 /***/ "./scss/index.js":
 /*!***********************!*\
   !*** ./scss/index.js ***!
@@ -94,18 +105,7 @@
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _index_scss__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./index.scss */ \"./scss/index.scss\");\n/* harmony import */ var _index_scss__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_index_scss__WEBPACK_IMPORTED_MODULE_0__);\n\n\n\n//# sourceURL=webpack:///./scss/index.js?");
-
-/***/ }),
-
-/***/ "./scss/index.scss":
-/*!*************************!*\
-  !*** ./scss/index.scss ***!
-  \*************************/
-/*! no static exports found */
-/***/ (function(module, exports, __webpack_require__) {
-
-eval("// extracted by mini-css-extract-plugin\n\n//# sourceURL=webpack:///./scss/index.scss?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _beta_index_scss__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./beta/index.scss */ \"./scss/beta/index.scss\");\n/* harmony import */ var _beta_index_scss__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_beta_index_scss__WEBPACK_IMPORTED_MODULE_0__);\n\n\n\n//# sourceURL=webpack:///./scss/index.js?");
 
 /***/ }),
 
--- scss/index.js
+++ scss/index.js
@@ -1,1 +1,1 @@
-import './index.scss'
+import './beta/index.scss'
