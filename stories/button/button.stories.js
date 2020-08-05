4dbef1584a40c4ef731d14451091f5872e18a0c7
Thiago Thimóteo <thiago.thimoteo@gmail.com>
2020-08-05 09:54:09 -0300

    create parseToCodeBlock helper

--- stories/button/button.stories.js
+++ stories/button/button.stories.js
@@ -1,50 +1,6 @@
 import { text, radios } from '@storybook/addon-knobs'
 import { withKnobs } from '@storybook/addon-knobs'
-import copyCodeBlock from '@pickra/copy-code-block'
-
-import hljs from 'highlight.js/lib/highlight'
-import xml from 'highlight.js/lib/languages/xml'
-hljs.registerLanguage('html', xml)
-
-const options = {
-  lang: 'html',
-  colors: {
-    background: '#222',
-    textColor: '#fff',
-    attr: '#fab',
-    string: '#abe',
-    tag: '#afa',
-    name: '#ea8'
-  },
-  cssOverrides: `
-    .container {
-      position: relative;
-      padding: 2em;
-      align-items: center;
-    }
-
-    .container button {
-      position: absolute;
-      bottom: -1px;
-      right: -1px;
-      padding: 3px 5px;
-      font-size: 12px;
-      font-weight: bold;
-      color: #999;
-      background-color: #eee !important;
-      border-radius: 4px 0 0 0;
-      outline: 0;
-    }
-
-    .container button:hover {
-      color: #666;
-    }
-
-    .container code {
-      padding-bottom: 0;
-    }
-  `
-}
+import { parseToCodeBlock } from '../../.storybook/helpers'
 
 export default {
   title: 'Components|Buttons',
@@ -55,7 +11,6 @@ export default {
     }
   }
 }
-
 
 export const Common = () => {
   const types = {
@@ -94,7 +49,7 @@ export const Common = () => {
 
       <br /><br />
 
-      ${copyCodeBlock(`<button class="${classes}">${buttonText}</button>`, options )}
+      ${parseToCodeBlock(`<button class="${classes}">${buttonText}</button>`)}
 
       <div class="ch-spaceStackGroup">
         <h3 class="ch-title ch-title--3">Exemplos</h3>
