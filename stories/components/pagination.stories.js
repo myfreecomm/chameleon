01669f23901c7f853a15719b02849c9c900bc083
Thiago Thimóteo <thiago.thimoteo@gmail.com>
2020-08-05 15:34:49 -0300

    Add pagination story

---
+++ stories/components/pagination.stories.js
@@ -0,0 +1,60 @@
+import { withKnobs, radios } from '@storybook/addon-knobs'
+import { insertTemplateOnContainer } from '../../.storybook/helpers'
+
+export default {
+  title: 'Components/Pagination',
+  decorators: [withKnobs],
+  parameters: {
+    docs: {
+      page: ''
+    }
+  }
+}
+
+const sizes = {
+  default: '',
+  small: 'ch-pagination--small'
+}
+
+export const Pagination = () => {
+  const paginationSizes = radios('Sizes', sizes, '')
+  const classes = `ch-pagination ${paginationSizes}`.trim()
+
+  const template = `
+    <nav class="${classes}">
+      <a href="#" class="ch-pagination-item ch-pagination-item--previous">
+        <i class="ch-icon"></i>
+      </a>
+
+      <a href="#" class="ch-pagination-item ch-pagination-item--first">
+        1
+      </a>
+
+      <button type="button" class="ch-pagination-item ch-pagination-item--gap">
+        <i class="ch-icon"></i>
+      </button>
+
+      <a href="#" class="ch-pagination-item" title="4">
+        4
+      </a>
+
+      <a href="#" class="ch-pagination-item" title="5">
+        5
+      </a>
+
+      <button type="button" class="ch-pagination-item ch-pagination-item--gap">
+        <i class="ch-icon"></i>
+      </button>
+
+      <a href="#" class="ch-pagination-item ch-pagination-item--last">
+        120
+      </a>
+
+      <a href="#" class="ch-pagination-item ch-pagination-item--next">
+        <i class="ch-icon"></i>
+      </a>
+    </nav>
+  `
+
+  return insertTemplateOnContainer(template, 'Pagination')
+}
