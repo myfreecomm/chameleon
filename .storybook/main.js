e96ffb81b6f698f5d92f5f7ab51ddfd9196c99ab
Thiago Thimóteo <thiago.thimoteo@gmail.com>
2020-08-07 09:49:56 -0300

    Add viewport addon

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,8 @@
 module.exports = {
   stories: ['../stories/**/*.stories.@(js|mdx)'],
-  addons: ['@storybook/addon-knobs/register', '@storybook/addon-docs']
+  addons: [
+    '@storybook/addon-knobs/register',
+    '@storybook/addon-docs',
+    '@storybook/addon-viewport/register'
+  ]
 };
